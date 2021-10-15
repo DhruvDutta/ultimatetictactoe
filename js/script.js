23dbd637410dba178acffded9454751d04c5fac3
DhruvDutta <dhrvdutta@gmail.com>
2021-10-15 15:41:01 +0530

    creating

--- js/script.js
+++ js/script.js
@@ -95,36 +95,36 @@ function Initiate_game(){
     const commentsRef = ref(db, 'room/' + curr_ref);
     firebase.onChildAdded(commentsRef, (data) => {
         win_check_fr()
-        if(isNumeric(data.key)){
-            if(parseInt(data.key)>0 && parseInt(data.key)<=100){
-                document.getElementById(data.key).innerText = data.val();
-                deck[data.val()].push(parseInt(data.key))
-                if(local['X'].includes(parseInt(data.key)%10) || local['O'].includes(parseInt(data.key)%10)){
-                    for(let i=1;i<10;i++){
-                        if(i==parseInt(data.key)%10){
-                            document.getElementsByClassName(`cover${i}`)[0].classList.remove('d-none')
-                            console.log('blur except:',i)
-                        }
-                        else if(document.getElementsByClassName(`cover${i}`)[0].innerText ==''){
-                            
-                            document.getElementsByClassName(`cover${i}`)[0].classList.add('d-none')
-                        }
-                    }
-                }else{
-                    for(let i=1;i<10;i++){
-                        if(i==parseInt(data.key)%10 ){
-                                console.log('blur except:',i)
-                                document.getElementsByClassName(`cover${i}`)[0].classList.add('d-none')
-                                continue
-                            }
-                            if(document.getElementsByClassName(`cover${i}`)[0].innerText ==''){
+        setTimeout(()=>{
+            if(isNumeric(data.key)){
+                if(parseInt(data.key)>0 && parseInt(data.key)<=100){
+                    document.getElementById(data.key).innerText = data.val();
+                    deck[data.val()].push(parseInt(data.key))
+                    if(local['X'].includes(parseInt(data.key)%10) || local['O'].includes(parseInt(data.key)%10)){
+                        for(let i=1;i<10;i++){
+                            if(i==parseInt(data.key)%10){
                                 document.getElementsByClassName(`cover${i}`)[0].classList.remove('d-none')
+                                console.log('blur except:',i)
                             }
+                            else if(document.getElementsByClassName(`cover${i}`)[0].innerText ==''){
+                                document.getElementsByClassName(`cover${i}`)[0].classList.add('d-none')
+                            }
+                        }
+                    }else{
+                        for(let i=1;i<10;i++){
+                            if(i==parseInt(data.key)%10 ){
+                                    console.log('blur except:',i)
+                                    document.getElementsByClassName(`cover${i}`)[0].classList.add('d-none')
+                                    continue
+                                }
+                                if(document.getElementsByClassName(`cover${i}`)[0].innerText ==''){
+                                    document.getElementsByClassName(`cover${i}`)[0].classList.remove('d-none')
+                                }
+                        }
                     }
                 }
-                
             }
-        }
+        },1500)
         if(data.key == 'o_user' && user_mark!='O'){
             oppo_name = data.val()
             if(oppo_name==''){
@@ -140,7 +140,6 @@ function Initiate_game(){
             }
             document.getElementById('oppoturn').innerText = `${oppo_name}`
         }
-        win_check_fr()
     });
     
     window.onbeforeunload = (e)=>{
